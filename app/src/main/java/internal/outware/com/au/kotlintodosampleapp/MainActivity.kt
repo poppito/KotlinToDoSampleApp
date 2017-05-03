4c9d17cc1decad8497c37d3e26749717a8707b0a
Harsh Overseer <harsh@embry.io>
2017-05-03 21:36:44 +1000

    First Empty kaitlin activity. fixed up gradle files to use Kotlin. Added manifest entry to make MainActivity actually main.

---
+++ app/src/main/java/internal/outware/com/au/kotlintodosampleapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package internal.outware.com.au.kotlintodosampleapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
