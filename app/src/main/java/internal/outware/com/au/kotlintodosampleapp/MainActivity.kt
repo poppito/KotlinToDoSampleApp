6e7db7b4ab44b8c66fd32b65de9bb3701d2cc90c
Harsh Overseer <harsh@embry.io>
2017-05-03 21:46:50 +1000

    HelloWorld works.

--- app/src/main/java/internal/outware/com/au/kotlintodosampleapp/MainActivity.kt
+++ app/src/main/java/internal/outware/com/au/kotlintodosampleapp/MainActivity.kt
@@ -1,12 +1,17 @@
 package internal.outware.com.au.kotlintodosampleapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val message = "Hello World!"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mHelloWorldText = findViewById(R.id.txt_hello_world) as TextView
+        mHelloWorldText.text = message
     }
 }
