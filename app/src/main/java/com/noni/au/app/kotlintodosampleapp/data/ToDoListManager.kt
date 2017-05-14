1f929619f2733daa8b70068e82ea3dea8a6e15a9
Harsh Overseer <harsh@embry.io>
2017-05-14 18:57:16 +1000

    fixes to manager class.

--- app/src/main/java/com/noni/au/app/kotlintodosampleapp/data/ToDoListManager.kt
+++ app/src/main/java/com/noni/au/app/kotlintodosampleapp/data/ToDoListManager.kt
@@ -1,9 +1,11 @@
 package com.noni.au.app.kotlintodosampleapp.data
 
+import android.content.ContentValues
 import android.content.Context
 import com.noni.au.app.kotlintodosampleapp.domain.ReminderRepository
 
 class ToDoListManager(val context: Context?) : ReminderRepository {
+
     val mContext = context
 
     val mDbHelper = ToDoListDbHelper(mContext)
@@ -11,7 +13,14 @@ class ToDoListManager(val context: Context?) : ReminderRepository {
     override fun getReminderById(id: String) {
     }
 
-    override fun createReminder() {
+    override fun createReminder(title: String, content: String?) {
+        val dB = mDbHelper.writableDatabase
+        val contentValues = ContentValues()
+        contentValues.put(ToDoListDbHelper.getTitleColumnName(), title)
+        if (content != null) {
+            contentValues.put(ToDoListDbHelper.getContentColumnName(), content)
+        }
+        dB.insert(ToDoListDbHelper.getToDoListTableName(), null, contentValues)
     }
 
     override fun removeReminder(id: String) {
--- app/src/main/java/com/noni/au/app/kotlintodosampleapp/domain/ReminderRepository.kt
+++ app/src/main/java/com/noni/au/app/kotlintodosampleapp/domain/ReminderRepository.kt
@@ -2,7 +2,7 @@ package com.noni.au.app.kotlintodosampleapp.domain
 
 interface ReminderRepository  {
     fun getReminderById(id : String)
-    fun createReminder()
+    fun createReminder(title: String, content: String?)
     fun removeReminder(id : String)
     fun createReminderDb()
 }
