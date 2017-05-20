a36149215303cd4aa03650ecf0e39922a2a52213
Harsh Overseer <harsh@embry.io>
2017-05-20 21:21:22 +1000

    added a bunch of input stuff i.e. textwatcher and updated imeoptions as well as kotlinx

--- app/src/main/java/com/noni/au/app/kotlintodosampleapp/presentation/CreateReminderPresenter.kt
+++ app/src/main/java/com/noni/au/app/kotlintodosampleapp/presentation/CreateReminderPresenter.kt
@@ -2,16 +2,21 @@ package com.noni.au.app.kotlintodosampleapp.presentation
 
 import com.noni.au.app.kotlintodosampleapp.base.BasePresenter
 import com.noni.au.app.kotlintodosampleapp.base.Presenter
+import com.noni.au.app.kotlintodosampleapp.domain.ReminderRepository
+import javax.inject.Inject
 
-class CreateReminderPresenter<ViewSurface>: BasePresenter<ViewSurface>(), Presenter<ViewSurface> {
+class CreateReminderPresenter<ViewSurface> @Inject constructor(repository: ReminderRepository): BasePresenter<ViewSurface>(), Presenter<ViewSurface> {
 
     var viewSurface : ViewSurface? = null
+    lateinit var repository: ReminderRepository
 
     override fun onViewAttached(view: ViewSurface) {
         super.onViewAttached(view)
         viewSurface = view
+        this.repository = repository
     }
 
     interface ViewSurface {
+
     }
 }
--- app/src/main/java/com/noni/au/app/kotlintodosampleapp/view/CreateReminderActivity.kt
+++ app/src/main/java/com/noni/au/app/kotlintodosampleapp/view/CreateReminderActivity.kt
@@ -2,10 +2,16 @@ package com.noni.au.app.kotlintodosampleapp.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import com.noni.au.app.kotlintodosampleapp.R
 import com.noni.au.app.kotlintodosampleapp.presentation.CreateReminderPresenter
+import kotlinx.android.synthetic.main.activity_create_reminder.*
 
-class CreateReminderActivity : AppCompatActivity(), CreateReminderPresenter.ViewSurface {
+class CreateReminderActivity : AppCompatActivity(), CreateReminderPresenter.ViewSurface, TextWatcher {
+
+    private val TAG = "createreminders"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +22,8 @@ class CreateReminderActivity : AppCompatActivity(), CreateReminderPresenter.View
     //region private
 
     private fun setWatchers() {
-
+        et_title.addTextChangedListener(this)
+        et_content.addTextChangedListener(this)
     }
 
     //endregion
@@ -28,6 +35,24 @@ class CreateReminderActivity : AppCompatActivity(), CreateReminderPresenter.View
     }
 
     // endregion
+
+    // region textwatcher
+
+    override fun afterTextChanged(s: Editable?) {
+        if (s?.hashCode() == et_title.text.hashCode()) {
+            Log.v(TAG, "title")
+        } else if (s?.hashCode() == et_content.hashCode()) {
+            Log.v(TAG, "content")
+        }
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        //not required but added by the textwatcher inteface
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        //not required but added by the textwatcher inteface
+    }
 
     //endregion
 }
