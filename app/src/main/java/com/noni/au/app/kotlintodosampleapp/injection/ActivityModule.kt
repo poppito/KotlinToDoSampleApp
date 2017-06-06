d719968236cf5c204e3518752d957a98be305f85
Harsh Overseer <harsh@embry.io>
2017-06-06 20:55:39 +1000

    refactor

--- app/src/main/java/com/noni/au/app/kotlintodosampleapp/injection/ActivityModule.kt
+++ app/src/main/java/com/noni/au/app/kotlintodosampleapp/injection/ActivityModule.kt
@@ -1,23 +1,8 @@
 package com.noni.au.app.kotlintodosampleapp.injection
 
-import android.content.Context
-import com.noni.au.app.kotlintodosampleapp.data.ToDoListManager
-import com.noni.au.app.kotlintodosampleapp.domain.facades.CreateReminderFacade
-import com.noni.au.app.kotlintodosampleapp.domain.repositories.ReminderRepository
-import com.noni.au.app.kotlintodosampleapp.presentation.presenters.CreateReminderPresenter
 import dagger.Module
-import dagger.Provides
 
 @PerScreen
 @Module (includes = arrayOf(AppModule::class))
 class ActivityModule() {
-    @Provides
-    fun getToDoPresenter(facade: CreateReminderFacade) : CreateReminderPresenter {
-        return CreateReminderPresenter(facade)
-    }
-
-    @Provides
-    fun getReminderRepositry(context: Context) : ReminderRepository {
-        return ToDoListManager(context)
-    }
 }
--- app/src/main/java/com/noni/au/app/kotlintodosampleapp/injection/FragmentModule.kt
+++ app/src/main/java/com/noni/au/app/kotlintodosampleapp/injection/FragmentModule.kt
@@ -1,6 +1,9 @@
 package com.noni.au.app.kotlintodosampleapp.injection
 
+import android.content.Context
+import com.noni.au.app.kotlintodosampleapp.data.ToDoListManager
 import com.noni.au.app.kotlintodosampleapp.domain.facades.CreateReminderFacade
+import com.noni.au.app.kotlintodosampleapp.domain.repositories.ReminderRepository
 import com.noni.au.app.kotlintodosampleapp.presentation.presenters.CreateReminderPresenter
 import dagger.Module
 import dagger.Provides
@@ -12,4 +15,10 @@ class FragmentModule {
     fun getCreateReminderPresenter(facade: CreateReminderFacade) : CreateReminderPresenter {
         return CreateReminderPresenter(facade)
     }
+    @Provides
+    fun getReminderRepository(context: Context) : ReminderRepository {
+        return ToDoListManager(context)
+    }
+    @Provides
+    fun getReminderFacade()
 }
