d560ad4d0a5faf0395c06c03957a980d5a5168c6
Harsh Overseer <harsh@embry.io>
2017-06-06 20:50:58 +1000

    add constructors and injection for facade/repo

--- app/src/main/java/com/noni/au/app/kotlintodosampleapp/domain/facades/CreateReminderFacade.kt
+++ app/src/main/java/com/noni/au/app/kotlintodosampleapp/domain/facades/CreateReminderFacade.kt
@@ -1,6 +1,11 @@
 package com.noni.au.app.kotlintodosampleapp.domain.facades
 
+import android.content.Context
+import com.noni.au.app.kotlintodosampleapp.domain.repositories.ReminderRepository
 import javax.inject.Inject
 
-class CreateReminderFacade @Inject constructor() {
+class CreateReminderFacade @Inject constructor(val repository : ReminderRepository, val context: Context) {
+    fun createAReminder(title: String, content: String) {
+        repository.createReminder(title, content)
+    }
 }
